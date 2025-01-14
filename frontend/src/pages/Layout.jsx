d8426ed06c7dc57d4ce453bfcc4fb193e85d2e29
Ishan-Gijavanekar <isg.sachin@gmail.com>
2025-01-14 12:02:06 +0530

    Last minute changes

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
-import Sidebar from './SideBar';
+import Sidebar from './Sidebar';
 import { useSidebar } from '../customhooks/useSidebar';
 
 const Layout = () => {
